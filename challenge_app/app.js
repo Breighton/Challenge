edcccfb6a9e7d8712f5237a4bdfd8b842b768545
vagrant <vagrant@ubuntu-xenial>
2017-05-04 17:51:37 +0000

    Flattened out callbacks and added code coverage

--- challenge_app/app.js
+++ challenge_app/app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const http = require('http');
 
-const lp= require('./routes/longestPreview')
+const lp = require('./routes/longestPreview')
 const about = require('./routes/about')
+const lp2 = require('./routes/testPreview')
 
 const app = express()
 
@@ -15,7 +16,7 @@ app.get('/terms/:tid/longest-preview-media-url', lp.find)
 app.get('/about', about.show)
 
 // *** server config *** //
-const server   = http.createServer(app);
+const server = http.createServer(app);
 server.listen(5000, function() {
   console.log('Challenge server started, listening on port 5000!');
 });
--- challenge_app/routes/longestPreview.js
+++ challenge_app/routes/longestPreview.js
@@ -3,86 +3,130 @@ const client = new Client();
 
 exports.find = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  console.log("Processing request: " + req.params.tid)
-  let args = {
-    path: { "tid": req.params.tid }, // path substitution var
-    headers: { "Accept": "application/json" }
+  console.log("Processing request: " + req.params.tid);
+
+  const fetchTermList = (initialTid) => {
+    return new Promise( function(resolve, reject) {
+      let args = {
+        path: { "tid": initialTid }, // path substitution var
+        headers: { "Accept": "application/json" }
+      };
+      client.get("http://d6api.gaia.com/vocabulary/1/${tid}", args,
+        termListFetch = (data, response) => {
+          if (response.statusCode != 200){
+            let errMsg = "Internal error while attempting to read d6api.gaia.com/vocabulary/1/${tid}, received code: "
+               + response.statusCode;
+             reject(errMsg);
+          }
+          else if (Object.keys(data.terms).length == 0){
+            let errMsg = "Internal error while attempting to read d6api.gaia.com/vocabulary/1/${tid}, terms key empty";
+            reject(errMsg);
+          }
+          else {
+            // get tid from first term in response
+            resolve(data.terms[0].tid);
+          }
+        }
+      )
+      .on('error', termListFetchError = (err) => {
+        let errMsg = "Internal error while attempting to reach d6api.gaia.com/vocabulary/1/${tid}, received message: "
+          + err.message;
+        reject(errMsg);
+      });
+    });
   };
 
-  client.get("http://d6api.gaia.com/vocabulary/1/${tid}", args,
-    termListFetch = (data, response) => {
-      if (response.statusCode != 200){
-        console.log('Error on reponse from /vocabulary', response.statusCode);
-        res.status(500).send('Internal error while attempting to read d6api.gaia.com/vocabulary/1/${tid}!')
-        return
-      }
-      // get tid from first term in response
-      let firstTermTid = data.terms[0].tid
-      //console.log(data);
-
+  const fetchLongestPreview = ( tid ) => {
+    return new Promise( function(resolve, reject) {
       let termArgs = {
-          path: { "tid": firstTermTid }, // path substitution var
+          path: { "tid": tid }, // path substitution var
           headers: { "Accept": "application/json" }
       };
       client.get("http://d6api.gaia.com/videos/term/${tid}", termArgs,
         videoListFetch = (data, response) => {
           // iterate through titles in response to find longest duration preview
           if (response.statusCode != 200){
-            console.log('Error on reponse from /videos', response.statusCode);
-            res.status(500).send('Internal error while attempting to read d6api.gaia.com/videos/term/${tid}!')
-            return
+            let errMsg = "Internal error while attempting to read d6api.gaia.com/videos/term/${tid}, received code: "
+              + response.statusCode;
+            reject(errMsg);
           }
-          // iterate through titles in response to find longest duration preview
-          let longestDuration = 0
-          let previewNid = -1
-          let titleNid = -1
-          let content = ''
-          data.titles.forEach((title) => {
-            if (typeof title.preview !== 'undefined') {
-              duration = parseInt(title.preview.duration, 10)
-              if (duration > longestDuration) {
-                  titleNid = parseInt(title.nid)
-                  longestDuration = duration
-                  previewNid = parseInt(title.preview.nid)
-                  content = title.preview.contentId
+          else if (Object.keys(data.titles).length == 0){
+            let errMsg = "Internal error while attempting to d6api.gaia.com/videos/term/${tid}, titles key empty";
+            reject(errMsg);
+          }
+          else {
+            // init return object
+            let previewData = {
+              "longestDuration": 0,
+              "previewNid": -1,
+              "titleNid": -1,
+              "content": ''
+            };
+            // iterate through titles in response to find longest duration preview
+            data.titles.forEach((title) => {
+              if (typeof title.preview !== 'undefined') {
+                duration = parseInt(title.preview.duration, 10)
+                // insure this video has a preview
+                if (duration > previewData.longestDuration) {
+                    previewData.titleNid = parseInt(title.nid);
+                    previewData.longestDuration = duration;
+                    previewData.previewNid = parseInt(title.preview.nid);
+                    previewData.content = title.preview.contentId;
+                }
               }
-            }
-          });
-
-          // read URL of longest preview
-          let mediaArgs = {
-              path: { "nid": previewNid}, // path substitution var
-              headers: { "Accept": "application/json" }
-          };
-          //return final URL given preview nid
-          client.get("http://d6api.gaia.com/media/${nid}", mediaArgs,
-            urlFetch = (data, response) => {
-              if (response.statusCode != 200){
-                console.log('Error on reponse from /media', response.statusCode);
-                res.status(500).send('Internal error while attempting to read d6api.gaia.com/media/{nid}!')
-                return
-              }
-              // form and reply with json response
-              res.json({
-                  'bcHLS' : data.mediaUrls.bcHLS,
-                  'titleNid' : titleNid,
-                  'previewNid' : previewNid,
-                  'previewDuration': longestDuration,
-                  'title' : content
-              });
-            }
-          )
-          .on('error', urlFetchError = (err) => {
-              console.log('Error on request to /media', err.request.options);
-                res.status(500).send('Internal error while attempting to reach d6api.gaia.com/media/{nid}!')
             });
-          }).on('error', videoListFetchError = (err) => {
-            console.log('Error on request to /videos', err.request.options);
-            res.status(500).send('Internal error while attempting to reach d6api.gaia.com/videos/term/${tid}!')
-          });
-      }).on('error', termListFetchError = (err) => {
-        console.log('Error on request to /vocabulary', err.request.options);
-        res.status(500).send('Internal error while attempting to reach d6api.gaia.com/vocabulary/1/${tid}!')
+            resolve(previewData);
+          }
+        }
+      )
+      .on('error', videoListFetchError = (err) => {
+        let errMsg = "Internal error while attempting to reach d6api.gaia.com/videos/1/${tid}, received message: "
+          + err.message;
+        reject(errMsg);
       });
-};
+    });
+  };
 
+  const fetchPreviewUrl = ( longestPreview ) => {
+    return new Promise( function(resolve, reject) {
+      // read URL of longest preview
+      let mediaArgs = {
+          path: { "nid": longestPreview.previewNid}, // path substitution var
+          headers: { "Accept": "application/json" }
+      };
+      //return final URL given preview nid
+      client.get("http://d6api.gaia.com/media/${nid}", mediaArgs,
+        urlFetch = (data, response) => {
+          if (response.statusCode != 200){
+            let errMsg = "Internal error while attempting to read d6api.gaia.com/media/{nid}, received code: "
+              + response.statusCode;
+            reject(errMsg);
+          }
+          else {
+            // form and reply with json response
+            res.json({
+                'bcHLS' : data.mediaUrls.bcHLS,
+                'titleNid' : longestPreview.titleNid,
+                'previewNid' : longestPreview.previewNid,
+                'previewDuration': longestPreview.longestDuration,
+                'title' : longestPreview.content
+            });
+          }
+        }
+      )
+      .on('error', urlFetchError = (err) => {
+        let errMsg = "Internal error while attempting to reach d6api.gaia.com/media/{nid, received message: "
+          + err.message;
+        reject(errMsg);
+      });
+    });
+  };
+
+  fetchTermList(req.params.tid)
+  .then( fetchLongestPreview )
+  .then( fetchPreviewUrl )
+  .catch((errMsg) => {
+    console.log(errMsg);
+    res.status(500).send(errMsg);
+  });
+};
